4ac53aa6342d559b6ced195f933e04297b53b52c
nutan kumari <nutananju240@gmail.com>
2024-03-11 10:45:58 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import { Box } from '@mui/material'
+
+function App() {
+ 
+
+  return (
+    <>
+   <Box>hello</Box>
+    </>
+  )
+}
+
+export default App
